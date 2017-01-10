b6566160f3cd6a408dff7d6d06c250b846666af3
Francisco de la Peña <frandelapena@gmail.com>
2017-01-10 16:43:45 +0000

    Another shot in the dark

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -43,14 +43,15 @@ source run_conda_forge_build_setup
 
 # Embarking on 2 case(s).
     set -x
-    export CONDA_PY=34
+    export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
-    export CONDA_PY=35
+    export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
 EOF
